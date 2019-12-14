bc17b500dbf45b2a0320b7ff5a4ba2be9ae99852
hangzili <hangzili@163.com>
2019-12-14 11:30:11 +0800

    lzh1214

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,20 @@ Route::get('/', function () {
 Route::any('/','IndexController@index');
 
 Route::any("goods/add","Admin\GoodsController@add");
+
+//前台收货地址添加
+Route::any("address/add","Admin\AddressController@add");
+Route::any("address/add_do","Admin\AddressController@add_do");
+Route::any("address/list","Admin\AddressController@list");
+//前台浏览记录
+Route::any("page/add","Admin\PageController@add");
+Route::any("page/list","Admin\PageController@list");
+//前台搜索记录
+Route::any("search/add","Admin\SearchController@add");
+Route::any("search/list","Admin\SearchController@list");
+//我的收藏
+Route::any("collection/add","Admin\CollectionController@add");
+Route::any("collection/list","Admin\CollectionController@list");
+//前台用户添加
+Route::any("user/add","Admin\UserController@add");
+Route::any("user/list","Admin\UserController@list");
