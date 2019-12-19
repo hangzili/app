d3cdfed77efeeb48e1df68887c55723efc71d8cd
hangzili <hangzili@163.com>
2019-12-19 18:47:10 +0800

    erwrwrw

--- app/Http/Controllers/Api/GoodslistController.php
+++ app/Http/Controllers/Api/GoodslistController.php
@@ -14,22 +14,22 @@ class GoodslistController extends Controller
     public function goods_list(Request $request)
     {
         $id = $request->all();
-        $goods = GoodsModel::where(['goods_id'=>10])->get()->toArray();
-        return json_encode(['code'=>200,'goodslistInfo'=>$goodslistInfo]);
+        $goodslistInfo = GoodsModel::where(['goods_id'=>$id])->get();
+        return json_encode($goodslistInfo);
     }
     //根据商品id获取商品属性
     public function goods_sku(Request $request)
     {
         $id = $request->all();
-        $goodsTypeInfo = BetModel::join('type','between.t_id','type.t_id')->where(['between.goods_id'=>$id])->get()->toArray();
-        return json_encode(['code'=>200,'goodsTypeInfo'=>$goodsTypeInfo]);
+        $goodsTypeInfo = BetModel::join('type','between.t_id','type.t_id')->where(['between.goods_id'=>$id])->get();
+        return json_encode($goodsTypeInfo);
     }
 
     // 根据属性id查询属性值
     public function type_attr(Request $request)
     {
         $id = $request->all();
-        $goodsAttrInfo = BetModel::join('attr','between.a_id','attr.a_id')->where(['between.goods_id'=>$id])->get()->toArray();
-        return json_encode(['code'=>200,'goodsAttrInfo'=>$goodsAttrInfo]);
+        $goodsAttrInfo = BetModel::join('attr','between.a_id','attr.a_id')->where(['between.goods_id'=>$id])->get();
+        return json_encode($goodsAttrInfo);
     }
 }
