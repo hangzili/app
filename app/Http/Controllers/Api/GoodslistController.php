90c893735168d1669736b0ef12107f361f3cd6b4
hangzili <hangzili@163.com>
2019-12-19 15:54:41 +0800

    ertre

---
+++ app/Http/Controllers/Api/GoodslistController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Model\GoodsModel;
+use App\Model\BetModel;
+use App\Model\AttrModel;
+use App\Model\TypeModel;
+class GoodslistController extends Controller
+{
+    //商品详情页展示商品
+    public function goods_list(Request $request)
+    {
+        $goods_id = $request->all('goods_id');
+        $goods_id = $goods_id['goods_id'];
+        $list = GoodsModel::where('goods_id',$goods_id)->first();
+        return json_decode($list);
+    }
+    //根据商品id获取商品属性
+    public function goods_sku(Request $request)
+    {
+        $goods_id = $request->all('goods_id');
+        $goods_id = $goods_id['goods_id'];
+        $type = BetModel::join('type','type.t_id','=','between.t_id')->where('between.goods_id','=',$goods_id)->get()->toArray();
+        $attrarr = array();
+        foreach($type as $k=>$v){
+            // echo $v['a_id'];
+            $attrarr[] = AttrModel::where('a_id','=',$v['a_id'])->get();
+            $type['attr']=$attrarr;
+        }
+        
+        return json_decode($type);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -149,6 +149,9 @@ Route::prefix('api')->group(function () {
     Route::any("brGroApi","Api\ApiController@brGroApi");//前台品牌商品接口
     Route::any("gdetailsApi","Api\ApiController@gdetailsApi");//前台商品详情接口
 });
+//前台商品详情页的接口
+Route::any("api/goods_list","Api\GoodslistController@goods_list");//前台商品详情接口
+Route::any("api/goods_sku","Api\GoodslistController@goods_sku");//前台商品详情sku接口
 
 
 
