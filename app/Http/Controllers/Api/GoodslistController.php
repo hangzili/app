a1a72c10360654c9890739036650146962a065d3
hangzili <hangzili@163.com>
2019-12-19 16:40:09 +0800

    lzy13

--- app/Http/Controllers/Api/GoodslistController.php
+++ app/Http/Controllers/Api/GoodslistController.php
@@ -15,7 +15,7 @@ class GoodslistController extends Controller
     {
         $goods_id = $request->all('goods_id');
         $goods_id = $goods_id['goods_id'];
-        $list = GoodsModel::where('goods_id',$goods_id)->first();
+        $list = GoodsModel::where('goods_id',$goods_id)->first()->toArray();
         return json_decode($list);
     }
     //根据商品id获取商品属性
