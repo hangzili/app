bd7eee432e427fe117e68e6076fb216415070e4b
qwf0417 <891875482@qq.com>
2019-12-24 19:06:04 +0800

    ssss

--- app/Http/Controllers/Api/ApisController.php
+++ app/Http/Controllers/Api/ApisController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Cookie;
 use App\Model\CarModel;
-use App\Model\GoodsModel;
 use App\Model\ColleModel;
 class ApisController extends Controller
 {
--- app/Http/Middleware/Rbac.php
+++ app/Http/Middleware/Rbac.php
@@ -26,32 +26,33 @@ class Rbac
         }else{
             // 如果有 把cookie取出来
             $u_name = \Session::get('username');
+//            dd($u_name);
             // 查询当前用户是什么角色
             $user = new HuserModel;
             $role = new UserRoleModel;
             $userAll = $user->where(['u_name'=>$u_name])->join("user_role","user_role.u_id","=","huser.u_id")
             ->first()->toArray();
-            // dd($userAll);
+//             dd($userAll);
             // 根据查询出来的角色查询该角色对应的权限
             $r_id = $userAll['r_id'];
             $model = new RolePermiModel;
             $role_powerAll = $model->where(['r_id'=>$r_id])->join('permi','permi.p_id','=','role_permi.p_id')->get()->toArray();
-            // dd($role_powerAll);
+//             dd($role_powerAll);
             // $url = request()->server();
             // dd($url);
             $power = new PermiModel;
             $data = "";
+            $url = request()->server('REDIRECT_URL');
             foreach($role_powerAll as $k =>$v)
             {
-                $url = request()->server('REDIRECT_URL');
-                // dd($url);
-                // $http = request()->server('HTTP_HOST');
-                // $header = "http://".$http.$url;
-                $path = $power::where(['p_id'=>$v['p_id']])->where(['p_url'=>$url])->first();
-                $data.=$path;
+//                 dd($url);
+                 //$http = request()->server('HTTP_HOST');
+                 //$header = "http://".$http.$url;
+//                 dd($header);
+                $path = $power::where(['p_id'=>$v['p_id']])->where(['p_url'=>$url])->get()->toArray();
             }
-            // dd($data);
-            if(empty($data)){
+//             dd($path);
+            if($data){
                 echo '<script>alert("您的权限不够，请联系管理员");window.location.href="/"</script>';
             }
         }
