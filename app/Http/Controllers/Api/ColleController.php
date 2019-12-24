5b913257e8bd3461c0e1c2bf79e8083cd8d22164
hangzili <hangzili@163.com>
2019-12-24 09:27:22 +0800

    sdfs

--- app/Http/Controllers/Api/ColleController.php
+++ app/Http/Controllers/Api/ColleController.php
@@ -15,8 +15,8 @@ class ColleController extends Controller
     // 前台收藏添加接口
     public function ColleApi(Request $requets)
     {
-        $id = $request->all('goods_id');
-        $goods_id  = $id['godos_id'];
+        $goods_id = $request->all('goods_id');
+        // $goods_id  = $id['godos_id'];
         $goodsInfo = GoodsModel::where(['goods_id'=>$goods_id])->get()->toArray();
         // dd($goodsInfo);  
         $id = $goodsInfo[0]['goods_id'];
