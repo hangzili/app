d52d7bc70f58da547fa8daee0a1c7248d790b47a
hangzili <hangzili@163.com>
2019-12-24 08:18:57 +0800

    dddd

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -206,8 +206,8 @@ class ApiController extends Controller
     public function session()
     {
         $id = \Session::get('user');
-        return json_encode($id);
-        // $user = UserModel::where(['u_id'=>$id])->get()->toArray();
-        // return json_encode($user);
+        // return json_encode($id);
+        $user = UserModel::where(['u_id'=>$id])->get()->toArray();
+        return json_encode($user);
     }
 }
